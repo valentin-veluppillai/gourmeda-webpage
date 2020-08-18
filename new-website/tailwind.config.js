0d8ada74970ee738da0857e139d7289f3eb2618f
M00nD3v <26168940+M00nD3v@users.noreply.github.com>
2020-08-18 15:19:07 +0200

    added website design files

---
+++ new-website/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+      'proprietary': '#810622',
+    }},
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/custom-forms'),
+  ]
+}
